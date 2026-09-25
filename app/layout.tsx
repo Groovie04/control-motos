ca53228094fc948bbde4b0843d59c4fd9715b32b
Groovie04 <luis.barboza.ridery@gmail.com>
2026-09-25 05:46:21 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,19 @@
-export const metadata = {
-  title: "Ruedda - Control de Arrendamiento",
-  description: "Sistema de control y gestión de arrendamiento de motos",
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Control de Arrendamiento - Ruedda',
+  description: 'Sistema de control de ventas y unidades registradas',
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
